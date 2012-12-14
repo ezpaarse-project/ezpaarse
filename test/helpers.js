5e64723c33ba3ddb3ef42e0dc668a3bc808ff815
Yannick <yannick.schurter@inist.fr>
2012-12-14 11:40:53 +0100

    function added to compare two arrays of objects

--- test/helpers.js
+++ test/helpers.js
@@ -1,28 +1,61 @@
 /*jslint node: true, maxlen: 100, maxerr: 50, indent: 2 */
 'use strict';
 
-var options = require('./options.js');
 var request = require('request');
-var fs = require('fs');
+var fs      = require('fs');
+var config  = require('../config.json');
+
+var host    = '127.0.0.1';
+var port    =  config.EZPAARSE_NODEJS_PORT;
+var url     = 'http://' + host + ':' + port;
 
 exports.get = function (path, callback) {
-  var url = options.url + (path ? path : '/');
   request({
       method: 'GET',
-      url: url
+      url: url + (path ? path : '/')
     }, callback);
 };
 
 exports.post = function (path, filePath, callback) {
   var opt = {
     method: 'POST',
-    url: options.url + (path ? path : '/')
+    url: url + (path ? path : '/')
   };
 
   if (filePath) {
-    var fileContent = fs.readFileSync(filePath);
+    var fileContent = fs.readFileSync(filePath, 'utf8');
     if (fileContent) { opt.body = fileContent; }
   }
   
   request(opt, callback);
 };
+
+function objectsAreSame(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  var objectsAreSame = true;
+  for(var property in object1) {
+    if(object1[property] !== object2[property]) {
+      objectsAreSame = false;
+      break;
+    }
+  }
+  return objectsAreSame;
+}
+
+exports.compareArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  array1.forEach(function (object1) {
+    var found = false;
+    array2.forEach(function (object2) {
+      if (objectsAreSame(object1, object2));
+    });
+    if (!found) {
+      return false;
+    }
+  });
+  return true;
+}
--- test/injection-test.js
+++ test/injection-test.js
@@ -6,26 +6,28 @@ var helpers = require('./helpers.js');
 var fs = require('fs');
 var should = require('should');
 
+var logFile = __dirname + '/dataset/sd.2012-11-30.300.log';
+var resultFile = __dirname + '/dataset/sd.2012-11-30.300.result.json';
+
 describe('Le serveur', function () {
   describe('recevant un log en POST sur /ws/', function () {
     it('renvoie un fichier d\'output correctement formatté', function (done) {
-      helpers.post('/ws/', __dirname + '/dataset/sd.2012-11-30.300.log',
+      helpers.post('/ws/', logFile,
       function (error, res, body) {
         if (!res) {
           throw new Error('L\'application n\'est pas lancée');
         }
         res.should.have.status(200);
         
-        var correctOutput = fs.readFileSync(__dirname +
-          '/dataset/sd.2012-11-30.300.result.json', 'UTF-8');
+        var correctOutput = fs.readFileSync(resultFile, 'UTF-8');
         var correctJson = JSON.parse(correctOutput);
         correctJson.should.be.a('object');
 
         var bodyJson = JSON.parse(body);
         bodyJson.should.be.a('object');
-        
-        /*should.deepEqual(bodyJson, correctJson,
-          'La réponse du serveur ne correspond pas au résultat attendu');*/
+
+        should.ok(helpers.compareArrays(bodyJson, correctJson),
+          'La réponse du serveur ne correspond pas au résultat attendu');
 
         done();
       });
