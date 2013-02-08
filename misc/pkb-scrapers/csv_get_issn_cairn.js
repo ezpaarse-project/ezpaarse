7b552faab4ff2c9c55ff5abbf3f0dfbaa80bad68
Stéphane Gully <stephane.gully@gmail.com>
2013-02-08 18:48:35 +0100

    read csv line by line with pause between each line

--- misc/pkb-scrapers/csv_get_issn_cairn.js
+++ misc/pkb-scrapers/csv_get_issn_cairn.js
@@ -3,6 +3,23 @@ var request  = require('request');
 //var request  = require('request').defaults({'proxy':'http://myproxy:8080'});
 var jsdom    = require('jsdom');
 var jquery   = require('jquery');
+var async    = require('async');
+
+var pause    = 1000; // in ms
+
+// read csv line by line with pause between each line
+var q = async.queue(function (task, callback) {
+  request.get(task.data.url, function (error, response, body) {
+    jsdom.env({ html: body }, function (err, window) {
+      var $ = jquery.create(window);
+      task.data.issn =  $('div.h5Typ2:eq(0)').text().trim().replace('I.S.S.N. ', '');
+      setTimeout(function () {
+        task.tcallback(null, task.data);
+        callback();
+      }, pause);
+    });
+  });
+}, 1);
 
 csv()
 .from('cairn.20130213.csv', {
@@ -14,14 +31,5 @@ csv()
   end: false
 })
 .transform(function (data, index, callback) {
-  console.log("function data with: " + data.url);
-  request.get(data.url, function (error, response, body) {
-    
-    jsdom.env({ html: body }, function (err, window) {
-      var $ = jquery.create(window);
-      data.issn =  $('div.h5Typ2:eq(0)').text().trim().replace('I.S.S.N. ', '');
-      callback(null, data);
-    });
-
-  });
+  q.push({ 'data': data, 'tcallback': callback });
 });
