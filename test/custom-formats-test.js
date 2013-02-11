14e16a654eba681a28b2e28891927873a410754e
Yannick Schurter <yannick.schurter@inist.fr>
2013-02-11 12:11:16 +0100

    test of custom formats improved (again)

--- test/custom-formats-test.js
+++ test/custom-formats-test.js
@@ -2,11 +2,11 @@
 /*global describe, it*/
 'use strict';
 
-var helpers       = require('./helpers.js');
-var should        = require('should');
+var helpers = require('./helpers.js');
+var should  = require('should');
 
-var custom1      = __dirname + '/dataset/custom-1.log';
-var custom2      = __dirname + '/dataset/custom-2.log';
+var logFile     = __dirname + '/dataset/sd.mini.log';
+var gzipLogFile = __dirname + '/dataset/sd.mini.log.gz';
 
 function sendRequest(logFile, headers, status, callback) {
   helpers.post('/ws/', logFile, headers, function (error, res, body) {
@@ -24,14 +24,14 @@ function sendRequest(logFile, headers, status, callback) {
 describe('The server', function () {
   describe('receives different log files on the HTTP POST /ws/ route', function () {
     it('and recognizes lines format using the HTTP headers', function (done) {
-      var headers = { 'ezPAARSE-LogFormat': '%s %b %h %l %u %t %r' };
-      sendRequest(custom1, headers, 200, function () {
-        headers['ezPAARSE-LogFormat'] = '%s %b %h %l %u %r %t';
-        sendRequest(custom1, headers, 400, function () {
-          headers['ezPAARSE-LogFormat'] = '%r %l %u %h %s %b %t';
-          sendRequest(custom2, headers, 200, function () {
-            headers['ezPAARSE-LogFormat'] = '%r %l %u %t %h %s %b';
-            sendRequest(custom2, headers, 400, done);
+      var headers = { 'ezPAARSE-LogFormat': '%h %l %u %t %r %s %b' };
+      sendRequest(logFile, headers, 200, function () {
+        headers['ezPAARSE-LogFormat'] = '%h %l %u %r %s %b %t';
+        sendRequest(logFile, headers, 400, function () {
+          headers['Content-Encoding'] = 'gzip';
+          sendRequest(gzipLogFile, headers, 400, function () {
+            headers['ezPAARSE-LogFormat'] = '%h %l %u %t %r %s %b';
+            sendRequest(gzipLogFile, headers, 200, done);
           });
         });
       });
