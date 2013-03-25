c8a5ed6da55ea2fc041bb44c90225a25c38286c2
Yannick Schurter <yannick.schurter@inist.fr>
2013-03-25 14:44:59 +0100

    added a test for the pkb manager

---
+++ test/pkb-test.js
@@ -0,0 +1,21 @@
+/*jslint node: true, maxlen: 100, maxerr: 50, indent: 2 */
+/*global describe, it*/
+'use strict';
+
+var should    = require('should');
+var PkbManager = require('../lib/pkbManager.js');
+
+describe('The pkb manager', function () {
+  it('works properly', function (done) {
+    var pkbManager = new PkbManager();
+    pkbManager.get('npg', function (pkb) {
+      should.exist(pkb);
+      var record = pkb.get('aps');
+      should.exist(record.issn);
+      should.exist(record.eissn);
+      record.issn.should.equal('1671-4083');
+      record.eissn.should.equal('1745-7254');
+      done();
+    });
+  });
+});
