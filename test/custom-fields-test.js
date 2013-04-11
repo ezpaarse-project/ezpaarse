0bc0e559777278cc38eb57d29a601ddfb0f119d9
Yannick Schurter <yannick.schurter@inist.fr>
2013-04-11 11:03:10 +0200

    tests for outputFields

--- test/custom-fields-test.js
+++ test/custom-fields-test.js
@@ -10,7 +10,7 @@ var logFile       = __dirname + '/dataset/customformat.log';
 
 describe('The server', function () {
   describe('receives a log on the HTTP POST / route with a specific log format', function () {
-    it('and sends back a CSV containings the fields specified by the custom format',
+    it('and sends back a CSV containing the fields specified by the custom format',
         function (done) {
       var headers = {
         'Accept'            : 'text/csv',
@@ -31,6 +31,67 @@ describe('The server', function () {
         var csvheader = body[0].split(';');
         csvheader.should.include('col1');
         csvheader.should.include('col2');
+        
+        var ec        = body[1].split(';');
+        ec.should.include('chucknorris');
+        ec.should.include('012');
+        ec.should.include('ABC');
+        
+        done();
+      });
+    });
+  });
+  describe('receives a log on the HTTP POST / route with given fields', function () {
+    it('and sends back a CSV containing only the given fields',
+        function (done) {
+      var headers = {
+        'Accept'            : 'text/csv',
+        'LogFormat-ezproxy' : '%u %{col1}<[0-9]+> "%r" %{col2}<[A-Z]+>',
+        'OutputFields'      : 'col1,col2'
+      };
+      helpers.post('/', logFile, headers, function (error, res, body) {
+        if (error) {
+          throw error;
+        }
+        if (!res) {
+          throw new Error('ezPAARSE is not running');
+        }
+        res.should.have.status(200);
+        
+        body = body.trim().split('\n');
+        should.ok(body.length == 2, 'One EC should be returned');
+        body[0].should.equal('col1;col2');
+        body[1].should.equal('012;ABC');
+        
+        done();
+      });
+    });
+  });
+  describe('receives a log on the HTTP POST / route with additional fields', function () {
+    it('and sends back a CSV containing the default fields and the given ones',
+        function (done) {
+      var headers = {
+        'Accept'            : 'text/csv',
+        'LogFormat-ezproxy' : '%u %{col1}<[0-9]+> "%r" %{col2}<[A-Z]+>',
+        'OutputFields'      : '+newCol1,newCol2'
+      };
+      helpers.post('/', logFile, headers, function (error, res, body) {
+        if (error) {
+          throw error;
+        }
+        if (!res) {
+          throw new Error('ezPAARSE is not running');
+        }
+        res.should.have.status(200);
+        
+        body = body.trim().split('\n');
+        should.ok(body.length == 2, 'One EC should be returned');
+        
+        var csvheader = body[0].split(';');
+        csvheader.should.include('col1');
+        csvheader.should.include('col2');
+        csvheader.should.include('newCol1');
+        csvheader.should.include('newCol2');
         
         var ec        = body[1].split(';');
         ec.should.include('chucknorris');
