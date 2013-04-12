ad9b2d14da5c71e1c32563a77a7c58e5352ba0f4
Yannick Schurter <yannick.schurter@inist.fr>
2013-04-12 14:32:28 +0200

    new test for JobID header

---
+++ test/job-logs-test.js
@@ -0,0 +1,31 @@
+/*jslint node: true, maxlen: 100, maxerr: 50, indent: 2 */
+/*global describe, it*/
+'use strict';
+
+var helpers = require('./helpers.js');
+var fs      = require('fs');
+var should  = require('should');
+
+var logFile     = __dirname + '/dataset/sd.2012-11-30.300.log';
+
+describe('The server', function () {
+  describe('receives a log on the HTTP POST / route', function () {
+    it('and correctly sends log headers', function (done) {
+      var headers = {
+        'Accept'        : 'application/json'
+      };
+      helpers.post('/', logFile, headers, function (error, res, body) {
+        if (error) {
+          throw error;
+        }
+        if (!res) {
+          throw new Error('ezPAARSE is not running');
+        }
+        res.should.have.status(200);
+        should.exist(res.headers['jobid'], 'The JobID header was not sent by the server');
+
+        done();
+      });
+    });
+  });
+});
