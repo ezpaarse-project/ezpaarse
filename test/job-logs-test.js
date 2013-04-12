0da4a677b0ae60d69d130236cba46cfe0b8c456e
Yannick Schurter <yannick.schurter@inist.fr>
2013-04-12 17:59:44 +0200

    changes on log tests

--- test/job-logs-test.js
+++ test/job-logs-test.js
@@ -6,13 +6,32 @@ var helpers = require('./helpers.js');
 var fs      = require('fs');
 var should  = require('should');
 
-var logFile     = __dirname + '/dataset/sd.mini.log';
+var logFile = __dirname + '/dataset/sd.mini.log';
 
 describe('The server', function () {
-  describe('receives a log on the HTTP POST / route', function () {
-    it('and correctly handles logs', function (done) {
+  describe('receives a log', function () {
+    it('and sends a job ID in headers', function (done) {
       var headers = {
-        'Accept'        : 'application/json'
+        'Accept' : 'application/json'
+      };
+      helpers.post('/', logFile, headers, function (error, res, body) {
+        if (error) {
+          throw error;
+        }
+        if (!res) {
+          throw new Error('ezPAARSE is not running');
+        }
+        res.should.have.status(200);
+        should.exist(res.headers['jobid'],
+          'The header "JobID" was not sent by the server');
+        done();
+      });
+    });
+  });
+  describe('receives a log', function () {
+    it('and correctly handles JobTraces.log', function (done) {
+      var headers = {
+        'Accept' : 'application/json'
       };
       helpers.post('/', logFile, headers, function (error, res, body) {
         if (error) {
@@ -26,6 +45,32 @@ describe('The server', function () {
         should.exist(jobID, 'The header "JobID" was not sent by the server');
         should.exist(res.headers['jobtraces'],
           'The header "JobTraces" was not sent by the server');
+
+        var logDir = __dirname + '/../tmp/logs/' + jobID;
+        should.ok(fs.existsSync(logDir),
+          'The log folder has not been created');
+        should.ok(fs.existsSync(logDir + '/jobtraces.log'),
+          'JobTraces.log has not been created');
+
+        done();
+      });
+    });
+  });
+  describe('receives a log', function () {
+    it('and sends all log-related headers', function (done) {
+      var headers = {
+        'Accept' : 'application/json'
+      };
+      helpers.post('/', logFile, headers, function (error, res, body) {
+        if (error) {
+          throw error;
+        }
+        if (!res) {
+          throw new Error('ezPAARSE is not running');
+        }
+        res.should.have.status(200);
+
+        // TODO make a specific test for each header
         should.exist(res.headers['jobunknownformats'],
           'The header "JobUnknownFormats" was not sent by the server');
         should.exist(res.headers['jobignoreddomains'],
@@ -34,13 +79,6 @@ describe('The server', function () {
           'The header "JobUnqualifiedECs" was not sent by the server');
         should.exist(res.headers['jobpkbmissecs'],
           'The header "JobPKBMissECs" was not sent by the server');
-
-        var logPath = __dirname + '/../tmp/logs/' + jobID;
-        should.ok(fs.existsSync(logPath),
-          'The log folder has not been created');
-        should.ok(fs.existsSync(logPath + '/jobtraces.log'),
-          'JobTraces.log has not been created');
-
         done();
       });
     });
