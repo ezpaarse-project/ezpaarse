f7d097f212f7e0c9ed8526d5275e069786fc8ef7
Stéphane Gully <stephane.gully@gmail.com>
2013-04-16 22:12:49 +0200

    Now "make jshint" is a test

---
+++ test/jshint-test.js
@@ -0,0 +1,19 @@
+/*jslint node: true, maxlen: 100, maxerr: 50, indent: 2 */
+/*global describe, it*/
+'use strict';
+
+var fs            = require('fs');
+var should        = require('should');
+var shell         = require('shelljs');
+
+describe('The "make jshint" command', function () {
+  it('returns 0 if source code respects coding rules ', function (done) {
+    shell.exec('make jshint', { silent: true, async: true }, function (code, output) {
+      if (code !== 0) {
+        console.error(output);
+      }
+      should.equal(code, 0);
+      done();
+    });
+  });
+});
