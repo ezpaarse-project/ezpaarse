30ec8bfb8ebae2bc88b0913f161a5670cd72ff73
Siguret Yohan <yohan.siguret@gmail.com>
2013-05-02 14:57:19 +0200

    examples of url adress in NPG's parser

--- platforms/npg/parser.js
+++ platforms/npg/parser.js
@@ -22,9 +22,11 @@ function parseUrl(url) {
     //result.numero = match[3];
     //result.revue  = match[5];
     if (match[4].toUpperCase() == "FULL") {
+      // example : http://www.nature.com.gate1.inist.fr/nature/journal/v493/n7431/full/493166a.html
       result.rtype = 'ARTICLE';
       result.mime = 'HTML';
     } else {
+      // example : http://www.nature.com.gate1.inist.fr/nature/journal/v493/n7431/pdf/493166a.pdf
       result.rtype = 'ARTICLE';
       result.mime = 'PDF';
     }
@@ -34,39 +36,48 @@ function parseUrl(url) {
     //result.numero = match[3];
     //result.revue  = match[5];
     if (match[4].toUpperCase() == "FULL") {
+      // example : http://www.nature.com.gate1.inist.fr/bonekey/knowledgeenvironment/2012/120613/bonekey2012109/full/bonekey2012109.html
       result.rtype = 'ARTICLE';
       result.mime = 'HTML';
     } else {
+      // example : http://www.nature.com.gate1.inist.fr/bonekey/knowledgeenvironment/2012/120613/bonekey2012109/pdf/bonekey2012109.html
       result.rtype = 'ARTICLE';
       result.mime = 'PDF';
     }
   } else if ((match = /\/([a-zA-Z0-9]+)\/journal\/v([0-9]*)\/n([a-zA-Z0-9]*)\/index.html/.exec(url)) !== null) {
+    // example http://www.nature.com.gate1.inist.fr/nature/journal/v493/n7431/index.html
     result.pid = match[1];
     //result.volume = match[3];
     //result.numero = match[3];
     result.rtype = 'TOC';
     result.mime = 'MISC';
   } else if ((match = /^\/([a-zA-Z0-9]+)\/index.html/.exec(path)) !== null) {
+    // example : http://www.nature.com.gate1.inist.fr/nature/index.html
     result.pid = match[1];
     result.rtype = 'TOC';
     result.mime = 'MISC';
   } else if ((match = /^\/([a-zA-Z0-9]+)\/archive\/index.html/.exec(path)) !== null) {
+    // example : http://www.nature.com/clpt/archive/index.html?showyears=2013-2011-#y2011
     result.pid = match[1];
     result.rtype = 'TOC';
     result.mime = 'MISC';
   } else if ((match = /\/([a-zA-Z0-9]+)\/current_issue.html/.exec(url)) !== null) {
+    // example : http://www.nature.com.gate1.inist.fr/nature/current_issue.html
     result.pid = match[1];
     result.rtype = 'TOC';
     result.mime = 'MISC';
   } else if ((match = /\/news\//.exec(url)) !== null) {
+    // example : http://www.nature.com/news/work-resumes-on-lethal-flu-strains-1.12266
     result.pid = 'nature';
     result.rtype = 'ARTICLE';
     result.mime = 'HTML';
   } else if ((match = /\/polopoly_fs\//.exec(url)) !== null) {
+    // example : http://www.nature.com/polopoly_fs/1.12266
     result.pid = 'nature';
     result.rtype = 'ARTICLE';
     result.mime = 'PDF';
   } else if ((match = /\/siteindex\/index.html/.exec(url)) !== null) {
+    // example : http://www.nature.com.gate1.inist.fr/siteindex/index.html
     result.rtype = 'TOC';
     result.mime = 'MISC';
   }
