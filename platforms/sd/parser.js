935ca646623982831d333bca47ce672dc3b0e6b6
Siguret Yohan <yohan.siguret@gmail.com>
2013-05-03 11:36:53 +0200

    add of unitid (commented) in SD's parser

--- platforms/sd/parser.js
+++ platforms/sd/parser.js
@@ -55,6 +55,7 @@ function parseUrl(url) {
           // example : http://pdn.sciencedirect.com.gate1.inist.fr/science?_ob=MiamiImageURL&_cid=282179&_user=4046392
           // &_pii=S221267161200100X&_check=y&_origin=browseVolIssue&_zone=rslt_list_item&_coverDate=2012-12-31
           // &wchp=dGLbVlB-zSkWz&md5=79a307d3c9bdbea6d6a6092d73c25545&pid=1-s2.0-S221267161200100X-main.pdf
+          // result.unitid = match[1] + '-' + match[2];
           result.issn = match[1] + '-' + match[2];
           result.rtype = 'ARTICLE';
           result.mime = 'PDF';
@@ -62,10 +63,12 @@ function parseUrl(url) {
           // example : http://pdn.sciencedirect.com.gate1.inist.fr/science?_ob=MiamiImageURL&_cid=276181&_user=4046392
           // &_pii=B9780122694400500017&_check=y&_origin=browse&_zone=rslt_list_item&_coverDate=1996-12-31
           // &wchp=dGLzVlV-zSkWz&md5=7e7ed3b95463e5438053bb62f487cf57&pid=3-s2.0-B9780122694400500017-main.pdf
+          // result.unitid = match[1] + match[2];
           result.pid = match[1] + match[2];
           result.rtype = 'BOOK';
           result.mime = 'PDF';
         } else {
+          // result.unitid = param._pii.substr(1, 4) + '-' + param._pii.substr(5, 4);
           result.issn = param._pii.substr(1, 4) + '-' + param._pii.substr(5, 4);
           // Set consultation type to PDF
           result.rtype = 'ARTICLE';
@@ -77,29 +80,35 @@ function parseUrl(url) {
   } else {
     if ((match = /\/science\/article\/pii\/S([0-9]{4})([0-9]{3}[0-9Xx])/.exec(url)) !== null) {
       // example : http://www.sciencedirect.com.gate1.inist.fr/science/article/pii/S2212671612001011
+      // result.unitid = match[1] + '-' + match[2];
       result.issn = match[1] + '-' + match[2];
       result.rtype = 'ARTICLE';
       result.mime = 'HTML';
     } else if ((match = /\/science\/publication\?issn=([0-9]{4})([0-9]{4})/.exec(url)) !== null) {
+      // result.unitid = match[1] + '-' + match[2];
       result.issn = match[1] + '-' + match[2];
       result.rtype = 'TOC';
       result.mime = 'MISC';
     } else if ((match = /\/science\/journal\/([0-9]{4})([0-9]{4})/.exec(url)) !== null) {
       // example : http://www.sciencedirect.com.gate1.inist.fr/science/journal/22126716
+      // result.unitid = match[1] + '-' + match[2];
       result.issn = match[1] + '-' + match[2];
       result.rtype = 'TOC';
       result.mime = 'MISC';
     } else if ((match = /\/science\/bookseries\/([0-9]{8})/.exec(url)) !== null) {
       // example : http://www.sciencedirect.com.gate1.inist.fr/science/bookseries/00652458
+      // result.unitid = match[1];
       result.pid   = match[1];
       result.rtype = 'BOOKSERIE';
       result.mime = 'MISC';
     } else if ((match = /\/science\/handbooks\/([0-9]{8})/.exec(url)) !== null) {
+      // result.unitid = match[1];
       result.pid   = match[1];
       result.rtype = 'HANDBOOK';
       result.mime = 'MISC';
     } else if ((match = /\/science\/book\/([0-9]{13})/.exec(url)) !== null) {
       // example : http://www.sciencedirect.com.gate1.inist.fr/science/book/9780122694400
+      // result.unitid = match[1];
       result.pid   = match[1];
       result.rtype = 'BOOK';
       result.mime = 'HTML'
