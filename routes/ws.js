cce392832ddd2ee68f8b74299f8dca17ce8380b2
pseudom <dominique.lechaudel@gmail.com>
2013-05-15 17:35:36 +0200

    adding not matching log line

--- routes/ws.js
+++ routes/ws.js
@@ -273,7 +273,7 @@ module.exports = function (app, domains, ignoredDomains) {
           if (!treatedLines) {
             badBeginning = true;
             lazy.emit('end');
-            logger.warn('Couln\'t recognize first line : aborted.');
+            logger.warn('Couln\'t recognize first line : aborted.', {line: line});
           }
         }
         report.inc('nb-lines-input');
