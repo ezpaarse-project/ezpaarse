2a3070afc75e87d7191086d35e8b14554c86f10e
Yannick Schurter <yannick.schurter@inist.fr>
2013-05-24 18:28:28 +0200

    form 80% finished

---
+++ public/js/form.js
@@ -0,0 +1,87 @@
+var jobid;
+var host = $(location).attr('protocol') + '//' + $(location).attr('host');
+
+$('#input-log-type').on('change', function() {
+  if ($('#input-log-type').val()) {
+    $('#input-log-format').prop("disabled", false);
+  } else {
+    $('#input-log-format').prop("disabled", true);
+    $('#input-log-format').prop("value", null);
+  }
+});
+
+$('#submit').on('click', function () {
+  jobid = uuid.v1();
+  logroute = host + '/' + jobid + '/';
+
+  var headers = {};
+  
+  if ($('#input-log-type').val() && $('#input-log-format').val()) {
+    headers['Log-Format-' + $('#input-log-type').val()] = $('#input-log-format').val();
+  }
+
+  if ($('#input-log-compression').val()) {
+    headers['Content-Encoding'] = $('#input-log-compression').val();
+  }
+
+  if ($('#input-result-format').val()) {
+    headers['Accept'] = $('#input-result-format').val();
+  }
+
+  if ($('#input-result-compression').val()) {
+    headers['Accept-Encoding'] = $('#input-result-compression').val();
+  }
+
+  if ($('#input-traces').val()) {
+    headers['Traces-Level'] = $('#input-traces').val();
+  }
+
+  if ($('#input-output-fields').val()) {
+    headers['Output-Fields'] = $('#input-output-fields').val();
+  }
+
+  console.log('', headers);
+  var data = new FormData(document.getElementById('form'));
+  $.ajax({
+    headers:     headers, 
+    type:        'PUT',
+    url:         '/' + jobid,
+    dataType:    'html',
+    data:        data,
+    cache:       false,
+    contentType: false,
+    processData: false,
+    'beforeSend': function() {
+      
+    },
+    'success': function(data) {
+    },
+    'error': function(jqXHR, textStatus, errorThrown) {
+      var error;
+
+      if (errorThrown) {
+        error = textStatus + ", " + errorThrown;
+      } else {
+        error = textStatus;
+      }
+
+      $("#error").text(error);
+
+      $('.alert-error').slideDown();
+    },
+    'complete': function(data) {
+    }
+  });
+
+  $('#report-btn').prop("href", logroute + 'job-report.html');
+
+  $('#content-form').slideUp(function () {
+    $('#content-result').slideDown();
+  });
+
+});
+
+$('#form').on('submit', function () {
+
+  return false;
+});
