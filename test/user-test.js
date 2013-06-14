ab110a74e82b6cf7caa1cc008e8b7463ced6f6f1
Yannick Schurter <yannick.schurter@inist.fr>
2013-06-14 18:36:25 +0200

    added a test for multiple extra user fields and residuals

--- test/user-test.js
+++ test/user-test.js
@@ -2,17 +2,19 @@
 /*global describe, it*/
 'use strict';
 
-var helpers       = require('./helpers.js');
-var fs            = require('fs');
-var should        = require('should');
+var helpers = require('./helpers.js');
+var fs      = require('fs');
+var should  = require('should');
 
-var resultMonoPlus  = require('./dataset/user-mono-plus-result.json');
-var resultMonoSpace = require('./dataset/user-mono-space-result.json');
-var resultMultiPlus = require('./dataset/user-multi-plus-result.json');
+var resultMonoPlus     = require('./dataset/user-mono-plus.result.json');
+var resultMonoSpace    = require('./dataset/user-mono-space.result.json');
+var resultMultiPlus    = require('./dataset/user-multi-plus.result.json');
+var resultMultipleMisc = require('./dataset/user-multiple-misc.result.json');
 
-var logMonoPlus   = __dirname + '/dataset/user-mono-plus.log';
-var logMonoSpace  = __dirname + '/dataset/user-mono-space.log';
-var logMultiPlus  = __dirname + '/dataset/user-multi-plus.log';
+var logMonoPlus     = __dirname + '/dataset/user-mono-plus.log';
+var logMonoSpace    = __dirname + '/dataset/user-mono-space.log';
+var logMultiPlus    = __dirname + '/dataset/user-multi-plus.log';
+var logMultipleMisc = __dirname + '/dataset/user-multiple-misc.log';
 
 describe('The server', function () {
   describe('receives a log on the HTTP POST / route with a user field', function () {
@@ -98,6 +100,44 @@ describe('The server', function () {
         var result = JSON.parse(body);
 
         should.ok(helpers.compareArrays(result, resultMultiPlus),
+          'ezPAARSE does not match the intended result');
+        
+        done();
+      });
+    });
+  });
+  describe('receives a log on the HTTP POST / route with multiple user fields', function () {
+    it('containing multivalue sub-fields and returns a JSON ' +
+      'containing the fields specified by the user(@04)',
+      function (done) {
+      var headers = {
+        'Accept'                       : 'application/json',
+        'Log-Format-ezproxy'           : '%h - %u %t "%r" %s %b %{user1}<[a-zA-Z0-9+]*>'
+                                       + ' "%{user2}<[a-zA-Z0-9 ]*>"',
+        'user-field0-src'              : 'user1',
+        'user-field0-dest-groupe1'     : 'etu|persecr|uncas|unautre',
+        'user-field0-dest-categorie1'  : '[0-9]{3}',
+        'user-field0-sep'              : '+',
+        'user-field0-residual'         : 'rest1',
+        'user-field1-src'              : 'user2',
+        'user-field1-dest-groupe2'     : 'Etudiant|Professeur|Secretaire',
+        'user-field1-dest-categorie2'  : 'FC|Y|M|C|A',
+        'user-field1-dest-composante2' : 'Droit|Lettre|Maths',
+        'user-field1-sep'              : 'space'
+      };
+      helpers.post('/', logMultipleMisc, headers, function (error, res, body) {
+        if (error) {
+          throw error;
+        }
+        if (!res) {
+          throw new Error('ezPAARSE is not running');
+        }
+        // fs.writeFileSync('gerard.result.json', body);
+        res.should.have.status(200);
+        
+        var result = JSON.parse(body);
+
+        should.ok(helpers.compareArrays(result, resultMultipleMisc),
           'ezPAARSE does not match the intended result');
         
         done();
