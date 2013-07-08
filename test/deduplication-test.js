9a9822c5f463dfed6cad6232d98ae2f079a487a8
Yannick Schurter <yannick.schurter@inist.fr>
2013-07-08 16:49:58 +0200

    deduplication test for login/ip

--- test/deduplication-test.js
+++ test/deduplication-test.js
@@ -7,9 +7,12 @@ var path    = require('path');
 var should  = require('should');
 var helpers = require('./helpers.js');
 
+var logFormat         = '%h %u %{session}<[a-zA-Z0-9\\-]+> %t "%r" %s';
 var multipleStatus    = path.join(__dirname, '/dataset/sd.multiple-status.log');
-var sessionLogFile    = path.join(__dirname, '/dataset/sd.doublons.log');
+var redundantLogFile  = path.join(__dirname, '/dataset/sd.doublons.log');
 var sessionResultFile = path.join(__dirname, '/dataset/sd.doublons-session.result.json');
+var loginResultFile   = path.join(__dirname, '/dataset/sd.doublons-login.result.json');
+var ipResultFile      = path.join(__dirname, '/dataset/sd.doublons-ip.result.json');
 
 describe('The server', function () {
   describe('receives a log with multiple HTTP status codes', function () {
@@ -39,21 +42,80 @@ describe('The server', function () {
     });
   });
   describe('receives a log with redundant consultations on the HTTP POST / route', function () {
-    it('and sends back a deduplicated output file (@02 @tdd)', function (done) {
+    it('and sends back a deduplicated output file using session (@02 @tdd)', function (done) {
+      var headers = {
+        'Accept'               : 'application/json',
+        'Log-Format-ezproxy'   : logFormat,
+        'Double-Click-HTML'    : 10,
+        'Double-Click-MISC'    : 20,
+        'Double-Click-PDF'     : 30,
+        'Double-Click-C-Field' : 'session',
+        'Double-Click-Strategy': 'CLI'
+      };
+      helpers.post('/', redundantLogFile, headers, function (err, res, body) {
+        if (!res) { throw new Error('ezPAARSE is not running'); }
+        if (err)  { throw err; }
+        res.should.have.status(200);
+
+        var correctOutput = fs.readFileSync(sessionResultFile, 'UTF-8');
+        var correctJson   = JSON.parse(correctOutput);
+        var bodyJson      = JSON.parse(body);
+
+        correctJson.should.be.a('object');
+        bodyJson.should.be.a('object');
+        should.ok(helpers.compareArrays(bodyJson, correctJson),
+          'Server\'s answer do not match the intended result');
+
+        done();
+      });
+    });
+  });
+  describe('receives a log with redundant consultations on the HTTP POST / route', function () {
+    it('and sends back a deduplicated output file using login (@02 @tdd)', function (done) {
       var headers = {
         'Accept'              : 'application/json',
         'Log-Format-ezproxy'  : '%h %u %{session}<[a-zA-Z0-9\\-]+> %t "%r" %s',
         'Double-Click-HTML'   : 10,
         'Double-Click-MISC'   : 20,
         'Double-Click-PDF'    : 30,
-        'Double-Click-C-Field': 'session'
+        'Double-Click-C-Field': 'session',
+        'Double-Click-Strategy': 'LCI'
       };
-      helpers.post('/', sessionLogFile, headers, function (err, res, body) {
+      helpers.post('/', redundantLogFile, headers, function (err, res, body) {
         if (!res) { throw new Error('ezPAARSE is not running'); }
         if (err)  { throw err; }
         res.should.have.status(200);
 
-        var correctOutput = fs.readFileSync(sessionResultFile, 'UTF-8');
+        var correctOutput = fs.readFileSync(loginResultFile, 'UTF-8');
+        var correctJson   = JSON.parse(correctOutput);
+        var bodyJson      = JSON.parse(body);
+
+        correctJson.should.be.a('object');
+        bodyJson.should.be.a('object');
+        should.ok(helpers.compareArrays(bodyJson, correctJson),
+          'Server\'s answer do not match the intended result');
+
+        done();
+      });
+    });
+  });
+  describe('receives a log with redundant consultations on the HTTP POST / route', function () {
+    it('and sends back a deduplicated output file using IP (@02 @tdd)', function (done) {
+      var headers = {
+        'Accept'              : 'application/json',
+        'Log-Format-ezproxy'  : '%h %u %{session}<[a-zA-Z0-9\\-]+> %t "%r" %s',
+        'Double-Click-HTML'   : 10,
+        'Double-Click-MISC'   : 20,
+        'Double-Click-PDF'    : 30,
+        'Double-Click-C-Field': 'session',
+        'Double-Click-Strategy': 'ILC'
+      };
+      helpers.post('/', redundantLogFile, headers, function (err, res, body) {
+        if (!res) { throw new Error('ezPAARSE is not running'); }
+        if (err)  { throw err; }
+        res.should.have.status(200);
+
+        var correctOutput = fs.readFileSync(ipResultFile, 'UTF-8');
         var correctJson   = JSON.parse(correctOutput);
         var bodyJson      = JSON.parse(body);
 
