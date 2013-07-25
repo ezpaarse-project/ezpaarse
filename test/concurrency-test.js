0506bcfbf9509a100150a581868b7e79f297223f
Yannick Schurter <yannick.schurter@inist.fr>
2013-07-25 15:28:06 +0200

    added short concurrency test

--- test/concurrency-test.js
+++ test/concurrency-test.js
@@ -11,16 +11,51 @@ var async    = require('async');
 
 var headers = {};
 
-var nbRequests  = 20;
-var maxDuration = 30;
-var minDuration = 15;
-var maxRate     = 900;
-var minRate     = 200;
-
 describe('The server', function () {
+  this.timeout(10000);
+  it('correctly handles short concurrent requests (@01)', function (done) {
+    var nbRequests  = 3;
+    var maxDuration = 5;
+    var minDuration = 2;
+    var maxRate     = 800;
+    var minRate     = 400;
+    
+    var getLaunchFunction = function () {
+      var launchRequest = function (callback) {
+        var rate     = Math.floor(Math.random() * (maxRate - minRate + 1) + minRate);
+        var duration = Math.floor(Math.random() * (maxDuration - minDuration + 1) + minDuration);
+        
+        logF.logFaker({ rate: rate, duration: duration }, function (stream) {
+          helpers.postPiped('/', headers, stream, function (err, res, body) {
+          
+            if (!res) { throw new Error('ezPAARSE is not running'); }
+            if (err)  { throw err; }
+            res.should.have.status(200);
+            should.ok(body !== '', 'One or more responses are empty');
+
+            callback(null);
+          });
+        });
+      };
+
+      return launchRequest;
+    }
+
+    var requests = [];
+    for (var i = 0; i < nbRequests; i++) {
+      requests.push(getLaunchFunction());
+    }
+
+    async.parallel(requests, done);
+  });
 
   this.timeout(0);
-  it('correctly handles concurrent requests (@01 @big)', function (done) {
+  it('correctly handles big concurrent requests (@02 @big)', function (done) {
+    var nbRequests  = 15;
+    var maxDuration = 30;
+    var minDuration = 20;
+    var maxRate     = 800;
+    var minRate     = 400;
 
     var getLaunchFunction = function () {
       var launchRequest = function (callback) {
