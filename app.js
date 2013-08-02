af1bd74e061820eb812c654ca24de9618bcf232b
Yannick Schurter <yannick.schurter@inist.fr>
2013-08-02 11:17:19 +0200

    unused variables removed in app.js

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ var config        = require('./config.json');
 var http          = require('http');
 var path          = require('path');
 var fs            = require('fs');
-var parsers       = require('./lib/init.js');
 var folderChecker = require('./lib/folderchecker.js');
 var FolderReaper  = require('./lib/folderreaper.js');
 var winston       = require('winston');
 var socketio      = require('socket.io');
-var usagemanager  = require('./lib/usagemanager.js');
+require('./lib/init.js');
 
 winston.addColors({ verbose: 'green', info: 'green', warn: 'yellow', error: 'red' });
 
