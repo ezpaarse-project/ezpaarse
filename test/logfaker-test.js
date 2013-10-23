aa8c30fde72f59c646d6fabddfe370c6d4a52dd7
Yannick Schurter <yannick.schurter@inist.fr>
2013-10-23 18:10:51 +0200

    re-add command line test for logfaker

--- test/logfaker-test.js
+++ test/logfaker-test.js
@@ -1,11 +1,32 @@
 /*global describe, it*/
 'use strict';
 
-var should = require('should');
-var Lazy   = require('lazy');
-var logF   = require('../lib/logfaker.js');
+var should      = require('should');
+var Lazy        = require('lazy');
+var shell       = require('shelljs');
+var PassThrough = require('stream').PassThrough;
+var logF        = require('../lib/logfaker.js');
 
 describe('logfaker', function () {
+  describe('called using command line with --nb=1', function () {
+    it('sends one line of log (@01)', function (done) {
+      var nblines = 0;
+      var child   = shell.exec(__dirname + '/../bin/logfaker --nb=1', {async: true, silent: true});
+      var pt      = new PassThrough();
+      var lazy    = new Lazy(pt);
+      child.stdout.pipe(pt);
+
+      lazy.lines
+        .map(String)
+        .map(function () {
+          nblines++;
+        });
+      lazy.on('end', function () {
+        should.equal(nblines, 1);
+        done();
+      });
+    });
+  });
   describe('called with nb=1', function () {
     it('sends one line of log (@01)', function (done) {
       var nblines = 0;
