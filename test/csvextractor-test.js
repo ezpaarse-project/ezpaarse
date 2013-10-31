5f753cb8655f429c26348cef7c4246e269139fd7
Stéphane Gully <stephane.gully@gmail.com>
2013-10-31 18:55:38 +0100

    Basic tests for csvextractor

---
+++ test/csvextractor-test.js
@@ -0,0 +1,32 @@
+/* jshint maxlen: 260 */
+/* global describe, it */
+'use strict';
+
+var csvextractor = require('../lib/csvextractor.js');
+
+describe('The csvextractor command', function () {
+
+  it('must be able to extract the pid column from a valid CSV file (@01)', function (done) {
+
+    var csvString = 'title;isbn;eisbn;pid\nee;ff;vv;ll';
+    csvextractor.extract(csvString, [], function (err, records) {
+      err.should.be.equal(null);
+      records.should.have.property('ll');
+      records.ll.should.have.property('title');
+      done();
+    }, { silent: false, key: 'pid' });
+
+  });
+
+  it('should not crash if parsing a not syntaxicaly valid CSV file (@02)', function (done) {
+
+    var csvString = 'title;isbn;eisbn;pid\nee;ff;vv;ll\n"The Farce of the Fart" and Other Ribaldries;9780812243239;9780812205015;9780812205015';
+    csvextractor.extract(csvString, [], function (err, records) {
+      records.should.have.property('ll');
+      err.should.not.be.equal(null);
+      done();
+    }, { silent: false, key: 'pid' });
+
+  });
+
+});
