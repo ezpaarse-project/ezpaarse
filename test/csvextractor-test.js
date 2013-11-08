2b6b8b8c50a71fd8734c31901299ea0f5fb9fce9
Yannick Schurter <yannick.schurter@inist.fr>
2013-11-08 17:12:03 +0100

    replace isbn by pisbn

--- test/csvextractor-test.js
+++ test/csvextractor-test.js
@@ -9,7 +9,7 @@ describe('The csvextractor command', function () {
 
   it('must be able to extract the pid column from a valid CSV file (@01)', function (done) {
 
-    var csvString = 'title;isbn;eisbn;pid\nee;ff;vv;ll';
+    var csvString = 'title;pisbn;eisbn;pid\nee;ff;vv;ll';
     csvextractor.extract(csvString, [], function (err, records) {
       should.ok(err === null);
       records.should.have.property('ll');
@@ -21,7 +21,7 @@ describe('The csvextractor command', function () {
 
   it('should not crash if parsing a not syntaxicaly valid CSV file (@02)', function (done) {
 
-    var csvString = 'title;isbn;eisbn;pid\nee;ff;vv;ll\n"The Farce of the Fart" and Other Ribaldries;9780812243239;9780812205015;9780812205015';
+    var csvString = 'title;pisbn;eisbn;pid\nee;ff;vv;ll\n"The Farce of the Fart" and Other Ribaldries;9780812243239;9780812205015;9780812205015';
     csvextractor.extract(csvString, [], function (err, records) {
       records.should.have.property('ll');
       should.ok(err !== null);
