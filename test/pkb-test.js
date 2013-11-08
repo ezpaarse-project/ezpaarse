01d11c1a31624db7a7348b27c56434a9788979b8
Yannick Schurter <yannick.schurter@inist.fr>
2013-11-08 16:44:40 +0100

    replace issn by pissn

--- test/pkb-test.js
+++ test/pkb-test.js
@@ -10,10 +10,10 @@ describe('The pkb manager', function () {
     pkbManager.get('npg', function (pkb) {
       should.exist(pkb);
       var record = pkb.get('aps');
-      should.exist(record.issn);
+      should.exist(record.pissn);
       should.exist(record.eissn);
       should.exist(record.title);
-      record.issn.should.equal('1671-4083');
+      record.pissn.should.equal('1671-4083');
       record.eissn.should.equal('1745-7254');
       record.title.should.equal('Acta Pharmacologica Sinica');
       done();
