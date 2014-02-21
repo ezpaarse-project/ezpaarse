357e705aef97466c2486cf2de9a5f49827f40ab3
Stéphane Gully <stephane.gully@gmail.com>
2014-02-21 17:50:22 +0100

    test that a broken network event during an ezpaarse process does not leave file descriptors open (test fails for the moment because of a bug)

---
+++ test/network-error-test.js
@@ -0,0 +1,63 @@
+/*global describe, it*/
+'use strict';
+
+var helpers       = require('./helpers.js');
+var fs            = require('fs');
+var path          = require('path');
+var should        = require('should');
+var logF          = require('../lib/logfaker.js');
+var lsof          = require('lsof');
+
+// to check the number of open file descriptors by the ezpaarse process
+// we need to know the pid of the process
+var ezpaarsePid = fs.readFileSync(__dirname + '/../ezpaarse.pid', 'utf8');
+
+describe('The server receives a log but network is cut during the transfer', function () {
+  it('should stop the job process and free file descriptors (@01)', function (done) {
+
+    var lsofBefore  = [];
+    var lsofAfter   = [];
+
+    // get the lsof value before the test
+    lsof.raw(ezpaarsePid, function (data) {
+      lsofBefore = data;
+      logF.logFaker({ rate: 500 }, function (logStream) {
+        var headers = { };
+        var reqStream = helpers.postPiped('/', headers, logStream);
+
+        // do nothing as long as the HTTP response
+        // is not starting to be received
+        reqStream.on('response', function (res)  {
+
+          // abort the http request quickly once the process is running
+          setTimeout(function () {
+            reqStream.abort();
+          }, 100);
+
+          // stop the test once the ezpaarse response is closed
+          res.on('end', function () {
+            // test lsof !
+            // get the lsof value before the test
+            lsof.raw(ezpaarsePid, function (data) {
+              lsofAfter = data;
+
+              // test the number of open files before the log processing is equal
+              // to the number of open file after the log processing
+              lsofAfter.should.be.instanceof(Array);
+              lsofBefore.should.be.instanceof(Array);
+              
+              // add a trace to help to understand which file descriptor is still open
+              if (lsofAfter.length != lsofBefore.length) {
+                console.error(lsofAfter);
+              }
+
+              should.equal(lsofAfter.length, lsofBefore.length);
+
+              done();
+            });          
+          });
+        });
+      });
+    });
+  });
+});
