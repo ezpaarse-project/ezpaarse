40d6715926ad10cbc78550752b80b8bd0052310b
Yannick Schurter <yannick.schurter@inist.fr>
2014-02-24 17:58:47 +0100

    check only REG and DIR for lsof

--- test/network-error-test.js
+++ test/network-error-test.js
@@ -11,6 +11,10 @@ var lsof          = require('lsof');
 // we need to know the pid of the process
 var ezpaarsePid = fs.readFileSync(__dirname + '/../ezpaarse.pid', 'utf8');
 
+function filterTypes(element) {
+  return /^(?:DIR|REG)$/i.test(element.type);
+}
+
 describe('The server receives a log but network is cut during the transfer', function () {
   it('should stop the job process and free file descriptors (@01)', function (done) {
 
@@ -19,42 +23,42 @@ describe('The server receives a log but network is cut during the transfer', fun
 
     // get the lsof value before the test
     lsof.raw(ezpaarsePid, function (data) {
-      lsofBefore = data;
+      lsofBefore = data.filter(filterTypes);
       logF.logFaker({ rate: 500 }, function (logStream) {
-        var headers = { };
+        var headers   = {};
         var reqStream = helpers.postPiped('/', headers, logStream);
 
         // do nothing as long as the HTTP response
         // is not starting to be received
         reqStream.on('response', function (res)  {
 
-          // abort the http request quickly once the process is running
-          setTimeout(function () {
-            reqStream.abort();
-          }, 100);
-
           // stop the test once the ezpaarse response is closed
           res.on('end', function () {
             // test lsof !
             // get the lsof value before the test
             lsof.raw(ezpaarsePid, function (data) {
-              lsofAfter = data;
+              lsofAfter = data.filter(filterTypes);
 
               // test the number of open files before the log processing is equal
               // to the number of open file after the log processing
               lsofAfter.should.be.instanceof(Array);
               lsofBefore.should.be.instanceof(Array);
-              
+
               // add a trace to help to understand which file descriptor is still open
               if (lsofAfter.length != lsofBefore.length) {
                 console.error(lsofAfter);
               }
 
               should.equal(lsofAfter.length, lsofBefore.length);
-
               done();
             });
           });
+
+          // abort the http request quickly once the process is running
+          setTimeout(function () {
+            // logStream.unpipe();
+            reqStream.abort();
+          }, 100);
         });
       });
     });
