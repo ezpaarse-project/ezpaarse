a0bb39604238a74e507e8f464009ba28163e816a
Yannick Schurter <yannick.schurter@inist.fr>
2014-03-27 14:27:06 +0100

    remove unnecessary injection

--- public/js/angular/controllers-process.js
+++ public/js/angular/controllers-process.js
@@ -3,6 +3,6 @@
 /* Controllers of the process page */
 
 angular.module('ezPAARSE.form-controllers')
-  .controller('ProcessCtrl', function ($scope, $rootScope, requestService) {
+  .controller('ProcessCtrl', function ($scope, requestService) {
     $scope.request = requestService.data;
   });
