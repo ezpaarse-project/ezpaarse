428048a19382b76e07469b357a78e68bf10dfffb
Yannick Schurter <yannick.schurter@inist.fr>
2014-04-02 14:11:42 +0200

    expanded report in anonymous view and print css

--- public/js/angular/controllers-anonymous.js
+++ public/js/angular/controllers-anonymous.js
@@ -3,12 +3,16 @@
 /* Controllers of the anonymous pages */
 
 angular.module('ezPAARSE.anonymous-controllers', [])
-  .controller('ReportCtrl', function ($scope, $stateParams, $http) {
+  .controller('ReportCtrl', function ($scope, $stateParams, $http, $anchorScroll, $timeout) {
 
     $http.get('/' + $stateParams.jobID + '/job-report.json')
       .success(function (data) {
-        if (angular.isObject(data)) { $scope.report = data; }
-        else { $scope.error = true; }
+        if (angular.isObject(data)) {
+          $scope.report = data;
+          $timeout($anchorScroll);
+        } else {
+          $scope.error = true;
+        }
       })
       .error(function () {
         $scope.error = true;
