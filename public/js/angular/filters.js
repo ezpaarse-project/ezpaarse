7f87f3dad6d32907a5ff440dda23d41884d04fdc
Yannick Schurter <yannick.schurter@inist.fr>
2014-04-10 17:38:37 +0200

    general as first report category

--- public/js/angular/filters.js
+++ public/js/angular/filters.js
@@ -2,22 +2,40 @@
 
 /* Filters */
 
-angular.module('ezPAARSE.filters', []).
-  filter('bytesToSize', function() {
-    return function(bytes) {
+angular.module('ezPAARSE.filters', [])
+  .filter('bytesToSize', function() {
+    return function (bytes) {
       var sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
       if (bytes == 0) return '0 B';
       var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
       return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
     };
-  }).
-  filter('localNumber', function() {
-    return function(str) {
+  })
+  .filter('localNumber', function() {
+    return function (str) {
       var number = parseInt(str, 10);
       if (isNaN(number)) {
         return '';
       } else {
         return number.toLocaleString();
       }
+    };
+  })
+  .filter('reportCategories', function () {
+    return function (report) {
+      var categories = [];
+      if (!report) { return categories; }
+
+      for (var key in report) {
+        if (key == 'general') { categories.unshift(key); }
+        else { categories.push(key); }
+      }
+      return categories;
+    };
+  })
+  .filter('reportElements', function () {
+    return function (report, category) {
+      if (!report) { return {}; }
+      return report[category];
     };
   });
