1347ca5b1e45938feb915523d53e814fb1a2bcca
Yannick Schurter <yannick.schurter@inist.fr>
2014-04-11 14:59:44 +0200

    traces in report page and loaders

--- public/js/angular/controllers-anonymous.js
+++ public/js/angular/controllers-anonymous.js
@@ -4,17 +4,46 @@
 
 angular.module('ezPAARSE.anonymous-controllers', [])
   .controller('ReportCtrl', function ($scope, $stateParams, $http, $anchorScroll, $timeout) {
+    $scope.tracesLoading = true;
+    $scope.reportLoading = true;
 
     $http.get('/' + $stateParams.jobID + '/job-report.json')
       .success(function (data) {
+        $scope.reportLoading = false;
         if (angular.isObject(data)) {
           $scope.report = data;
           $timeout($anchorScroll);
         } else {
-          $scope.error = true;
+          $scope.reportError = true;
         }
       })
       .error(function () {
-        $scope.error = true;
+        $scope.reportLoading = false;
+        $scope.reportError   = true;
+      });
+
+    function toJsonArray(data) {
+      if (!data) { return []; }
+
+      var logs = [];
+      data.split('\n').forEach(function (line) {
+        try {
+          logs.push(JSON.parse(line));
+        } catch (e) {
+          return;
+        }
+      });
+
+      return logs;
+    }
+
+    $http.get('/' + $stateParams.jobID + '/job-traces.log', { transformResponse: toJsonArray })
+      .success(function (data) {
+        $scope.tracesLoading = false;
+        $scope.traces        = data;
+      })
+      .error(function () {
+        $scope.tracesLoading = false;
+        $scope.tracesError   = true;
       });
   });
