ec0a1833634037ba8b66f7f4b7a732ca5241009d
Yannick Schurter <yannick.schurter@inist.fr>
2014-05-19 17:42:50 +0200

    fix typo

--- public/js/angular/services.js
+++ public/js/angular/services.js
@@ -46,7 +46,7 @@ angular.module('ezPAARSE.services', [])
           { cat: 'rejets',  key: 'nb-lines-unordered-ecs',   percent: 0, title: 'rejects+chrono_anomalies' },
           { cat: 'rejets',  key: 'nb-lines-ignored-domains', percent: 0, title: 'rejects+ignored_domains' },
           { cat: 'rejets',  key: 'nb-lines-unknown-domains', percent: 0, title: 'rejects+unknown_domains' },
-          { cat: 'rejets',  key: 'nb-lines-unknown-format',  percent: 0, title: 'rejects+unknown_formats' },
+          { cat: 'rejets',  key: 'nb-lines-unknown-formats', percent: 0, title: 'rejects+unknown_formats' },
           { cat: 'rejets',  key: 'nb-lines-unqualified-ecs', percent: 0, title: 'rejects+unqualified_ecs' },
           { cat: 'rejets',  key: 'nb-lines-pkb-miss-ecs',    percent: 0, title: 'rejects+missing_pkbs' }
         ]
