d50289f0c26b286294401f279b08a9371e31ac10
Yannick Schurter <yannick.schurter@inist.fr>
2014-10-07 17:38:10 +0200

    add test for pkb-cleaner

--- test/pkb-test.js
+++ test/pkb-test.js
@@ -1,8 +1,11 @@
-/*global describe, it*/
+/*global describe, it, before*/
 'use strict';
 
-var should    = require('should');
+var should     = require('should');
+var fs         = require('fs');
+var path       = require('path');
 var pkbManager = require('../lib/pkbmanager.js');
+var pkbClean   = require('../lib/pkb-cleaner.js');
 
 describe('The pkb manager', function () {
   it('works properly (@01)', function (done) {
@@ -16,6 +19,45 @@ describe('The pkb manager', function () {
       record.online_identifier.should.equal('1745-7254');
       record.publication_title.should.equal('Acta Pharmacologica Sinica');
       done();
+    });
+  });
+});
+
+describe('The pkb cleaner', function () {
+  var pkbDir = path.join(__dirname, 'pkb');
+  var oldPkb = path.join(pkbDir, 'pkb_2014-03-11.txt');
+  var newPkb = path.join(pkbDir, 'pkb_2014-09-17.txt');
+
+  before(function (next) {
+    fs.mkdir(pkbDir, function (err) {
+      if (err && err.code != 'EEXIST') { throw err; }
+      fs.writeFile(oldPkb, 'title_id\n1\n2\n3\n4\n5\n', function (err) {
+        if (err) { throw err; }
+        fs.writeFile(newPkb, 'title_id\n3\n4\n5\n6\n7\n', function (err) {
+          if (err) { throw err; }
+          next();
+        });
+      });
+    });
+  });
+
+  it('correctly remove duplicates (@02)', function (done) {
+
+    pkbClean({ dir: pkbDir, rewrite: true })
+    .on('error', function (err) { throw err; })
+    .on('end', function () {
+
+      fs.readFile(oldPkb, function (err, oldContent) {
+        should.not.exist(err);
+        oldContent.toString().trim().split('\n').length.should.equal(3);
+
+        fs.readFile(newPkb, function (err, newContent) {
+          should.not.exist(err);
+          newContent.toString().trim().split('\n').length.should.equal(6);
+
+          done();
+        });
+      });
     });
   });
 });
