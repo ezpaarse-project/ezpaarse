c6b6853f8983ea5def0445e066e412810becb71a
Yannick Schurter <yannick.schurter@inist.fr>
2014-10-13 18:06:49 +0200

    add alerts config by headers

--- test/alerts-test.js
+++ test/alerts-test.js
@@ -47,4 +47,43 @@ describe('Alert', function () {
 
   });
 
+  it('should not be spawned if headers contains a high rate of activation (@02)', function (done) {
+
+    // prepare known and unknown lines of log
+    var knownDomainLog   = '247.63.228.176 - TEO [30/Nov/2012:00:13:10 +0100] "GET http://pdn.sciencedirect.com HTTP/1.1" 200 444';
+    var unknownDomainLog = '247.63.228.176 - TEO [30/Nov/2012:00:13:10 +0100] "GET http://www.unknowndomain.com HTTP/1.1" 200 444';
+
+    // create a stream and write 1100 lines of logs into it
+    // with about 13% of www.unknowndomain.com
+    var stream = new StreamPT();
+    var i = 0;
+    while (i < 1100) {
+      if (i % 8 === 0) { // write about 13% of www.unknowndomain.com
+        stream.write(unknownDomainLog + "\n");
+      } else {
+        stream.write(knownDomainLog + "\n");
+      }
+      i++;
+    }
+    stream.end();
+
+    // send the logs to ezPAARSE
+    helper.postPiped('/', { "Alerts-Unknown-Domains-Rate": 50 }, stream, function (err, res) {
+
+      res.should.have.status(200);
+      res.headers.should.have.property('job-id');
+
+      // download the ezpaarse job report
+      // to check the alerts section
+      helper.get('/' + res.headers['job-id'] + '/job-report.json', function (err, res) {
+        res.body = JSON.parse(res.body);
+        res.body.should.have.property('alerts');
+        res.body.alerts.should.have.property('active-alerts');
+        res.body.alerts.should.not.have.property('alert-1');
+        done();
+      });
+    });
+
+  });
+
 });
