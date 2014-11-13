eab237b8580db49a99ea67fd0e4674867f91ae4e
Yannick Schurter <yannick.schurter@inist.fr>
2014-11-13 11:15:26 +0100

    unknown domains now downloadable

--- routes/info.js
+++ routes/info.js
@@ -335,6 +335,21 @@ module.exports = function (app) {
   });
 
   /**
+   * GET route on /info/domains/unknown
+   */
+  app.get('/info/domains/unknown', function (req, res) {
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    fs.readFile(path.join(__dirname, '../domains.miss.csv'), function (err, data) {
+      if (err) { return res.status(err.code == 'ENOENT' ? 404 : 500).end(); }
+
+      res.status(200).send(data);
+    });
+  });
+
+  /**
    * GET route on /info/domains/:domain:
    */
   app.get(/\/info\/domains\/([a-zA-Z0-9\-\.]+)/, function (req, res) {
@@ -368,6 +383,7 @@ module.exports = function (app) {
       res.status(404).end();
     }
   });
+
 
   /**
    * GET route on /info/usage
