7bc51823f7e00ab9b2ae705327ebe2302a50ffce
Yannick Schurter <yannick.schurter@inist.fr>
2015-01-28 17:10:29 +0100

    validates JSON files

--- test/jshint-test.js
+++ test/jshint-test.js
@@ -5,13 +5,21 @@ var should = require('should');
 var path   = require('path');
 var spawn  = require('child_process').spawn;
 
-describe('The "make jshint" command', function () {
+describe('jsHint', function () {
   this.timeout(10000);
-  it('returns 0 if source code respects coding rules (@01)', function (done) {
+  it('validates javascript files (@01)', function (done) {
     var child = spawn('make', ['jshint'], { cwd: path.join(__dirname, '..') });
 
     child.on('exit', function (code) {
       should.equal(code, 0, 'code was not validated by jsHint');
+      done();
+    });
+  });
+  it('validates JSON files (@02)', function (done) {
+    var child = spawn('make', ['jsonhint'], { cwd: path.join(__dirname, '..') });
+
+    child.on('exit', function (code) {
+      should.equal(code, 0, 'JSON was not validated by jsHint');
       done();
     });
   });
