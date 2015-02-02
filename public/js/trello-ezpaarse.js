6dc203b88229fc318ddde863c8299c05bcb014c6
Yannick Schurter <yannick.schurter@inist.fr>
2015-02-02 14:06:53 +0100

    trello script refactoring

--- public/js/trello-ezpaarse.js
+++ public/js/trello-ezpaarse.js
@@ -1,215 +1,106 @@
+/**
+ * Api key utilisée pour pouvoir interroger
+ * en AJAX l'API REST de Trello
+ */
+function getTrelloApiKey() {
+  return '7fa507c389612aa4ca03f781cf2a8242';
+}
 
-    /**
-     * Parse les détails d'une plateforme
-     * depuis la description d'une carte Trello
-     */
-    function getPlatformDataFromTrelloCard(cardId, cb) {
-      var cardUrl = 'https://api.trello.com/1/cards/' + cardId + '?fields=desc&key=' + getTrelloApiKey();
-      $.get(cardUrl, function (card) {
-        var res = {};
-        var regexpAnalyseUrl = new RegExp('(http://analogist.couperin.org/platforms/[^ $\n]+)');
-        var regexpGitHubPlatform = new RegExp('(https://github.com/ezpaarse-project/ezpaarse-platforms/[^ $\n]+)');
-        var match;
-        if (match = card.desc.match(regexpAnalyseUrl)) {
-          res.analysisUrl = match[1];
-        }
-        if (match = card.desc.match(regexpGitHubPlatform)) {
-          res.gitHubUrl = match[1];
-        }
-        cb(null, res);
-      });
-    }
+/**
+ * Retourne l'id du board d'ezPAARSE sur Trello
+ *
+ */
+function getTrelloBoardId() {
+  return 'wEaLnz8d';
+}
 
-    /**
-     * Api key utilisée pour pouvoir interroger
-     * en AJAX l'API REST de Trello
-     */
-    function getTrelloApiKey() {
-      return '7fa507c389612aa4ca03f781cf2a8242';
-    }
+/**
+ * Retourne les listes présentes dans un board Trello
+ * https://trello.com/docs/api/board/index.html#get-1-boards-board-id-lists
+ */
+function getTrelloBoardLists(cb) {
+  var boardId   = getTrelloBoardId();
+  var platforms = [];
+  var boardUrl  = 'https://api.trello.com/1/boards/'
+    + boardId
+    + '/lists?key='
+    + getTrelloApiKey();
 
-    /**
-     * Retourne l'id du board d'ezPAARSE sur Trello
-     * 
-     */
-    function getTrelloBoardId() {
-      return 'wEaLnz8d';
-    }
+  $.get(boardUrl, function (lists) {
+    cb(null, lists);
+  });
+}
 
-    /**
-     * Retourne les listes présentes dans un board Trello
-     * https://trello.com/docs/api/board/index.html#get-1-boards-board-id-lists
-     */
-    function getTrelloBoardLists(boardId, cb) {
+function getTrelloPlatformsList(cb) {
+  var boardID    = getTrelloBoardId();
+  var listsURL   = 'https://api.trello.com/1/boards/' + boardID + '/lists?cards=all&key=' + getTrelloApiKey();
+  var membersURL = 'https://api.trello.com/1/boards/' + boardID + '/members?key=' + getTrelloApiKey();
+
+  $.get(listsURL)
+  .fail(function () {
+    cb(new Error('failed to get board lists'));
+  })
+  .done(function (lists) {
+    if (!Array.isArray(lists) || lists.length === 0) { return cb(null, []); }
+
+    $.get(membersURL)
+    .fail(function () {
+      cb(new Error('failed to get board members'));
+    })
+    .done(function (members) {
+      var indexedMembers = {};
+      members.forEach(function (m) { indexedMembers[m.id] = m.fullName; });
+
       var platforms = [];
-      var boardUrl = 'https://api.trello.com/1/boards/'
-        + boardId
-        + '/lists?key='
-        + getTrelloApiKey();
 
-      $.get(boardUrl, function (lists) {
-        cb(null, lists);
-      });
-    }
+      lists.forEach(function (list) {
+        list.cards.forEach(function (card) {
+          if (!Array.isArray(card.idMembers)) { card.idMembers = []; }
 
-    function getTrelloPlatformsList(cb) {
-      var platformsList = {};
-      getTrelloBoardLists(getTrelloBoardId(), function (err, lists) {
-        var nbList = lists.length;
-        if (nbList === 0) { return cb({}) };
-        $.each(lists, function (idX, trelloList) {
-          // puis la liste des plateformes présentes
-          // dans chaque listes du trello
-          getPlatformsFromTrelloList(trelloList,
-            function (err, platforms) {
-              nbList--;
-              platformsList[trelloList.pos] = platforms;
-              if (nbList === 0) {
-                cb(platformsList);
-              }
-            }
-          );
-        });
-      });
-    }
-
-
-    /**
-     * Retourne la liste des plateformes présentes
-     * dans une liste Trello
-     */
-    function getPlatformsFromTrelloList(trelloList, cb) {
-      var platforms = [];
-      var listUrl = 'https://api.trello.com/1/lists/'
-        + trelloList.id
-        + '/cards?fields=url,name,dateLastActivity,idMembers&key='
-        + getTrelloApiKey();
-      $.get(listUrl, function (cards) {
-        if (cards.length === 0) cb(null, []);
-        getUsersFromTrelloCards(cards, function (err, users) {
-          var nbCbCalled = 0;
-          var platforms = [];
-          $.each(cards, function (idx, trelloCard) {
-            nbCbCalled++;
-            getPlatformFromTrelloCard(trelloCard, users, function (err, platform) {
-              // retire  le contenu des parenthèses dans le nom des colonnes
-              platform.platformStatus = trelloList.name.replace(new RegExp(' *\\([^)]+\\)'), '');
-              // n'affiche pas la carte modèle
-              // https://trello.com/c/rlb3YezS/3-nom-pltfrme-modele-a-copier
-              if (platform.card.id !== '53fc86672dbb930194716829') {
-                platforms.push(platform);
-              }
-              nbCbCalled--;
-              if (nbCbCalled === 0) {
-                // tri des plateformes par dates
-                //platforms = platforms.sort(function (p1, p2) {
-                //  return p1.card.dateLastActivity < p2.card.dateLastActivity;
-                //});
-                platforms = platforms.sort(function (p1, p2) {
-                  return p1.card.name > p2.card.name ? 1: -1;
-                });
-                return cb(null, platforms);
-              }
-            });
+          card.members = card.idMembers.map(function (member) {
+            return indexedMembers[member];
           });
-        });
-      });
-    }
 
-    /**
-     * Retourne la liste des plateformes présentes
-     * dans une liste Trello
-     */
-    function getPlatformsFromTrelloBoard(cb) {
-      var platforms = [];
-      var listUrl = 'https://api.trello.com/1/boards/'
-        + getTrelloBoardId()
-        + '/cards?fields=url,shortUrl,name,dateLastActivity,idMembers&key='
-        + getTrelloApiKey();
-      $.get(listUrl, function (cards) {
-        if (cards.length === 0) cb(null, []);
-        getUsersFromTrelloCards(cards, function (err, users) {
-          var nbCbCalled = 0;
-          var platforms = [];
-          $.each(cards, function (idx, trelloCard) {
-            nbCbCalled++;
-            console.log(trelloCard);
-            getPlatformFromTrelloCard(trelloCard, users, function (err, platform) {
-
-              // retire  le contenu des parenthèses dans le nom des colonnes
-              //platform.platformStatus = trelloList.name.replace(new RegExp(' *\\([^)]+\\)'), '');
-              // n'affiche pas la carte modèle
-              // https://trello.com/c/rlb3YezS/3-nom-pltfrme-modele-a-copier
-              if (platform.card.id !== '53fc86672dbb930194716829') {
-                platforms.push(platform);
-              }
-              nbCbCalled--;
-              if (nbCbCalled === 0) {
-                // tri des plateformes par dates
-                //platforms = platforms.sort(function (p1, p2) {
-                //  return p1.card.dateLastActivity < p2.card.dateLastActivity;
-                //});
-                platforms = platforms.sort(function (p1, p2) {
-                  return p1.card.name > p2.card.name ? 1: -1;
-                });
-                return cb(null, platforms);
-              }
-            });
-          });
-        });
-      });
-    }
-
-
-
-    /**
-     * Retourne les détails d'une plateforme depuis une carte Trello
-     */
-    function getPlatformFromTrelloCard(card, users, cb) {
-      var platform = {};
-      var itemMembers = [];
-      $.each(card.idMembers, function (idx2, memberId) {
-        itemMembers.push(users[memberId].fullName);
-      });
-      platform.platformContact = itemMembers.join (', ');
-      platform.platformName = card.name;
-      platform.platformTrelloCard = card.url;
-      platform.platformTrelloUrl = card.shortUrl;
-      platform.platformDate = new Date(card.dateLastActivity).toLocaleDateString();
-      platform.card = card;
-
-      getPlatformDataFromTrelloCard(card.id, function (err, cardData) {
-        platform.platformAnalysisUrl = cardData.analysisUrl;
-        platform.platformGitHubUrl   = cardData.gitHubUrl;
-        cb(null, platform);
-      });
-    }
-
-    /**
-     * Retoure la liste des détails des utilisateurs 
-     * assignés à une carte Trello
-     */
-    function getUsersFromTrelloCards(cards, cb) {
-      // dédoublonnage de tous les users a récupérer
-      var userIds = [];
-      $.each(cards, function (idx, card) {
-        $.each(card.idMembers, function (idx2, userId) {
-          if (userIds.indexOf(userId) === -1) {
-            userIds.push(userId);
+          // exclut la carte modèle
+          if (card.id !== '53fc86672dbb930194716829') {
+            platforms.push(createPlatform(card, list));
           }
         });
       });
-      // récupération des info de chaque users
-      var users = {};
-      var nbCbCalled = 0;
-      if (userIds.length === 0) return cb(null, users);
-      $.each(userIds, function (idx, userId) {
-        var trelloUserUrl = 'https://api.trello.com/1/members/' + userId + '?key='+getTrelloApiKey();
-        nbCbCalled++;
-        $.get(trelloUserUrl, function (user) {
-          users[userId] = user;
-          nbCbCalled--;
-          if (nbCbCalled == 0) return cb(null, users);
-        });
+
+      platforms = platforms.sort(function (p1, p2) {
+        return p1.platformName.toLowerCase() > p2.platformName.toLowerCase() ? 1: -1;
       });
-    }
+
+      cb(null, platforms);
+    });
+  });
+}
+
+/**
+ * Retourne les détails d'une plateforme depuis une carte Trello
+ */
+function createPlatform(card, list) {
+  var platform = {
+    platformStatus:     list.name.replace(/\s*\([^\)]+\)/, ''),
+    platformContact:    card.members.join(', '),
+    platformName:       card.name,
+    platformTrelloCard: card.url,
+    platformTrelloUrl:  card.shortUrl,
+    platformDate:       new Date(card.dateLastActivity).toLocaleDateString(),
+    card:               card
+  };
+
+
+  var regexpAnalyseUrl     = new RegExp('(http://analogist.couperin.org/platforms/[^ $\n]+)');
+  var regexpGitHubPlatform = new RegExp('(https://github.com/ezpaarse-project/ezpaarse-platforms/[^ $\n]+)');
+  var match;
+  if (match = card.desc.match(regexpAnalyseUrl)) {
+    platform.platformAnalysisUrl = match[1];
+  }
+  if (match = card.desc.match(regexpGitHubPlatform)) {
+    platform.platformGitHubUrl = match[1];
+  }
+
+  return platform;
+}
