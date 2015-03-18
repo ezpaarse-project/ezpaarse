c099496f204c1e81b6e67e76a92134ac07134eab
Yannick Schurter <yannick.schurter@inist.fr>
2015-03-18 16:51:06 +0100

    add a test for data without linebreaks

---
+++ test/no-linebreak-test.js
@@ -0,0 +1,41 @@
+/*global describe, it*/
+'use strict';
+
+var PassThrough = require('stream').PassThrough;
+var helpers     = require('./helpers.js');
+
+describe('The server receives data with no line breaks', function () {
+  it('and should stop the process (@01)', function (done) {
+
+    var stream      = new PassThrough();
+    var gotResponse = false;
+    var canWrite    = true;
+
+    helpers.postPiped('/', {}, stream, function (err, res, body) {
+      gotResponse = true;
+      stream.end();
+
+      if (!res) { throw new Error('ezPAARSE is not running'); }
+      if (err)  { throw err; }
+      res.should.have.status(400);
+
+      res.headers.should.have.property('ezpaarse-status');
+      res.headers.should.have.property('ezpaarse-status-message');
+      var status = res.headers['ezpaarse-status'];
+      status.should.equal('4022', 'expected status 4022, got ' + status);
+
+      done();
+    });
+
+    // Try to send 100000 bytes of text
+    var i = 5000;
+    (function writeSomedata() {
+      if (gotResponse || --i < 0) { return; }
+
+      canWrite = stream.write('20 bytes long string');
+
+      if (canWrite) { setImmediate(writeSomedata); }
+      else { stream.once('drain', writeSomedata); }
+    })();
+  });
+});
