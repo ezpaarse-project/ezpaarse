224b2f78ece773203cb58e0abc8ac289ce4227e8
Yannick Schurter <yannick.schurter@inist.fr>
2015-05-29 11:00:48 +0200

    minor adjustments in tests

--- test/realtime-test.js
+++ test/realtime-test.js
@@ -8,14 +8,13 @@ var logLine  = '16.2.255.24 - 13BBIU1158 [01/Aug/2013:16:56:36 +0100] "GET http:
 
 describe('Real time ECs', function () {
 
-  it('should be generated when Double-Click-Removal is false and ezPAARSE-Buffer-Size equals zero (@01)', function (done) {
+  it('should be generated when Double-Click-Removal is false (@01)', function (done) {
 
     // create a stream and write 2 lines logs into it
     var stream = new StreamPT();
 
     var opt = {
-      'Double-Click-Removal': false,
-      'ezPAARSE-Buffer-Size': 0
+      'Double-Click-Removal': false
     };
 
     // send the logs to ezPAARSE
