75654f9ae8e4a7fe251bbbda33d8501c7754e7a4
pseudom <dominique.lechaudel@gmail.com>
2015-06-09 18:24:30 +0200

    remove echo messages

--- misc/windows/ezpaarse-docker-start.sh
+++ misc/windows/ezpaarse-docker-start.sh
@@ -3,15 +3,6 @@ set -e
 
 source ezpaarse-docker-common.sh
 
-#set Path=%Path%;C:\Program Files (x86)\Git\bin
-
-export TEST="mon test"
-#export PATH="$PATH"
-
-echo "$PATH"
-echo "$TEST"
-echo "$PATH"
-
 set +e
 echo 'starting...'
 boot2docker.exe start  > $LOGMSG 2> $LOGMSGERR
