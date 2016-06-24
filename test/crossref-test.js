f51456339270dbbf5a67be2286d3eaa411121298
pseudom <dominique.lechaudel@gmail.com>
2016-06-24 16:54:09 +0200

    Change crossref buffersize to 1

--- test/crossref-test.js
+++ test/crossref-test.js
@@ -8,54 +8,43 @@ var helpers = require('./helpers.js');
 
 var logFile = path.join(__dirname, 'dataset/es.doi.log');
 
-function testCrossref(callback) {
-  var headers = {
-    'Accept': 'application/json',
-    'crossref-Enrich': 'true'
-  };
+describe('crossref consultations', function () {
+  this.timeout(5000);
+  it('should be correctly enriched (@01)', function (done) {
+    var headers = {
+      'Accept': 'application/json',
+      'crossref-Enrich': 'true',
+      'crossref-buffer-size' : '1'
+    };
+    helpers.post('/', logFile, headers, function (err, res, body) {
+      if (!res) { throw new Error('ezPAARSE is not running'); }
+      if (err)  { throw err; }
+      res.statusCode.should.equal(200, 'expected 200, got ' + res.statusCode);
 
-  helpers.post('/', logFile, headers, function (err, res, body) {
-    if (!res) { throw new Error('ezPAARSE is not running'); }
-    if (err)  { throw err; }
-    res.statusCode.should.equal(200, 'expected 200, got ' + res.statusCode);
+      var result = JSON.parse(body);
+      result.should.have.length(2);
 
-    var result = JSON.parse(body);
-    result.should.have.length(2);
+      var ec = result[0];
 
-    var ec = result[0];
+      should.equal(ec['doi-DOI'], '10.1210/mn1.9781936704842');
+      should.equal(ec['doi-publisher'], 'The Endocrine Society');
 
-    should.equal(ec['doi-DOI'], '10.1210/mn1.9781936704842');
-    should.equal(ec['doi-publisher'], 'The Endocrine Society');
-
-
-    var reportURL = res.headers['job-report'];
-    should.exist(reportURL, 'The header "Job-Report" was not sent by the server');
-
-    helpers.get(reportURL, function (error, response, reportBody) {
-      if (!response) { throw new Error('ezPAARSE is not running'); }
-      if (error)     { throw error; }
-      response.statusCode.should.equal(200,
+      var reportURL = res.headers['job-report'];
+      should.exist(reportURL, 'The header "Job-Report" was not sent by the server');
+      helpers.get(reportURL, function (error, response, reportBody) {
+        if (!response) { throw new Error('ezPAARSE is not running'); }
+        if (error)     { throw error; }
+        response.statusCode.should.equal(200,
           'failed to get the report, server responded with a code ' + response.statusCode);
-
-      var report = JSON.parse(reportBody);
-      report.should.have.property('general');
-      report.general.should.have.property('crossref-queries');
-      report.general['crossref-queries'].should.be.type('number');
-      report.general['crossref-queries'].should.be.below(2,
+        var report = JSON.parse(reportBody);
+        report.should.have.property('general');
+        report.general.should.have.property('crossref-queries');
+        report.general['crossref-queries'].should.be.type('number');
+        report.general['crossref-queries'].should.be.below(2,
           'too many Crossref requests, one at least should be cached');
 
-      callback();
-    });
-  });
-
-}
-
-
-describe('crossref consultations', function () {
-  this.timeout(15000);
-  it('should be correctly enriched (@01)', function (done) {
-    testCrossref(function() {
-      testCrossref(done);
+        done();
+      });
     });
   });
 });
