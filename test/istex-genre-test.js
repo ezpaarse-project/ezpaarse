53413b507f09c2640a8f32cf2438c493963a353e
aloukili <abdel@dctchad.com>
2016-09-23 14:39:06 +0200

    change name of file log for test istex genre

--- test/istex-genre-test.js
+++ test/istex-genre-test.js
@@ -6,7 +6,7 @@ var should  = require('should');
 var helpers = require('./helpers.js');
 
 
-var logFile = path.join(__dirname, 'dataset/test-rtype-istex.log');
+var logFile = path.join(__dirname, 'dataset/istex-rtype-test.log');
 
 describe('istex consultations rtype', function () {
   it('should be correctly enriched (@01)', function (done) {
