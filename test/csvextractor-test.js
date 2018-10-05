4aa86e5ae99bd48ac4409e4673e3d47f6a4fb893
Yannick Schurter <yannick.schurter@gmail.com>
2018-10-05 14:05:45 +0200

    fix csvextractor tests

--- test/csvextractor-test.js
+++ test/csvextractor-test.js
@@ -2,14 +2,14 @@
 /* global describe, it */
 'use strict';
 
-var csvextractor = require('../lib/csvextractor.js');
-var should       = require('should');
+const csvextractor = require('../lib/csvextractor.js');
+const should = require('should');
 
 describe('The csvextractor command', function () {
 
-  it('must be able to extract the pid column from a valid CSV file (@01)', function (done) {
+  it('must be able to extract the title_id column from a CSV file (@01)', function (done) {
 
-    var csvString = 'publication_title;print_identifier;online_identifier;title_id\nee;ff;vv;ll';
+    const csvString = 'publication_title;print_identifier;online_identifier;title_id\nee;ff;vv;ll';
     csvextractor.extract(csvString, { silent: false, key: 'title_id' }, function (err, records) {
       should.ok(err === null);
       records.should.have.property('ll');
@@ -19,12 +19,11 @@ describe('The csvextractor command', function () {
 
   });
 
-  it('should not crash if parsing a not syntaxicaly valid CSV file (@02)', function (done) {
+  it('should not crash when parsing an invalid CSV file (@02)', function (done) {
 
-    var csvString = 'publication_title;print_identifier;online_identifier;title_id\nee;ff;vv;ll\n"The Farce of the Fart" and Other Ribaldries;9780812243239;9780812205015;9780812205015';
+    const csvString = 'publication_title;print_identifier;online_identifier;title_id\nee;ff;vv;ll\n"The Farce of the Fart" and Other Ribaldries;9780812243239;9780812205015;9780812205015';
     csvextractor.extract(csvString, { silent: false, key: 'title_id' }, function (err, records) {
-      records.should.have.property('ll');
-      should.ok(err !== null);
+      should.exist(err);
       done();
     });
 
