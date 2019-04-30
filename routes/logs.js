19cd3cef4d1b5a751731a9b7dbe7313598cf7136
Yannick Schurter <yannick.schurter@gmail.com>
2019-04-30 11:25:26 +0200

    fix html report

--- routes/logs.js
+++ routes/logs.js
@@ -1,23 +1,57 @@
 'use strict';
 
-var fs     = require('fs-extra');
-var path   = require('path');
-var moment = require('moment');
-var Boom   = require('boom');
+const fs     = require('fs-extra');
+const path   = require('path');
+const moment = require('moment');
+const Boom   = require('boom');
+const ejs   = require('ejs');
 
-var { Router } = require('express');
-var app = Router();
+const { Router } = require('express');
+const app = Router();
 
-var jobidPattern = '^/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})';
+const jobidPattern = '^/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})';
+
+const reportTemplate = `
+<html>
+  <head>
+    <title><%= title %></title>
+    <style type="text/css">
+      table { border-collapse: collapse; margin-top: 20px; }
+      td, th { border: 1px solid black; padding: 4px; min-width: 50px; text-align: left; }
+      th { background-color: #B8E5FF; }
+      td:first-child { background-color: #DCF1FD; }
+    </style>
+  </head>
+  <body>
+    <h1>Rapport d'exécution</h1>
+    <table class="table table-bordered table-hover">
+    <% for (let group in report) { %>
+      <tr><th colspan="2"><%= group[0].toUpperCase() + group.substring(1) %></th></tr>
+
+      <% for (let key in report[group]) { %>
+      <tr id="<%= key %>">
+        <td><%= key %></td>
+        <% if (/^http/.test(report[group][key])) { %>
+          <td><a href="<%= report[group][key] %>"><%= report[group][key] %></a></td>
+        <% } else { %>
+          <td><%= report[group][key] %></td>
+        <% } %>
+      </tr>
+      <% } %>
+    <% } %>
+    </table>
+  </body>
+</html>
+`;
 
 /**
 * GET route on /:rid/job-report.{html|json}
 * Used to get a report file
 */
 app.get(new RegExp(jobidPattern + '/job-report\\.(html|json)$'), function (req, res, next) {
-  var requestID  = req.params[0];
-  var format     = req.params[1];
-  var reportFile = path.join(__dirname, '/../tmp/jobs/',
+  const requestID  = req.params[0];
+  const format     = req.params[1];
+  const reportFile = path.join(__dirname, '/../tmp/jobs/',
     requestID.charAt(0),
     requestID.charAt(1),
     requestID,
@@ -38,7 +72,7 @@ app.get(new RegExp(jobidPattern + '/job-report\\.(html|json)$'), function (req, 
       fs.readFile(reportFile, function (err, data) {
         if (err) { return next(err); }
 
-        var report;
+        let report;
 
         try {
           report = JSON.parse(data);
@@ -46,13 +80,14 @@ app.get(new RegExp(jobidPattern + '/job-report\\.(html|json)$'), function (req, 
           return next(err);
         }
 
-        var title = 'Rapport d\'exécution';
+        let title = 'Execution report';
         if (report.general && report.general['Job-Date']) {
-          title += ' (' + moment(report.general['Job-Date']).format('DD-MM-YYYY hh[h]mm') + ')';
+          const jobDate = moment(report.general['Job-Date']).format('DD-MM-YYYY hh[h]mm');
+          title += ` (${jobDate})`;
         }
         title += ' - ezPAARSE';
-        // Rapport d’exécution (25-06-2013 11h25) - ezPAARSE
-        res.render('report-standalone', { report: report, title: title });
+        // Execution report (25-06-2013 11h25) - ezPAARSE
+        res.send(ejs.render(reportTemplate, { report, title }));
       });
       break;
 
@@ -68,9 +103,9 @@ app.get(new RegExp(jobidPattern + '/job-report\\.(html|json)$'), function (req, 
 */
 app.get(new RegExp(jobidPattern + '/([a-zA-Z0-9\\-_]+(?:\\.[a-z]{2,4}){1,2})$'),
   function (req, res, next) {
-    var requestID = req.params[0];
-    var filename  = req.params[1];
-    var logFile   = path.join(__dirname, '/../tmp/jobs/',
+    const requestID = req.params[0];
+    const filename  = req.params[1];
+    const logFile   = path.join(__dirname, '/../tmp/jobs/',
       requestID.charAt(0),
       requestID.charAt(1),
       requestID,
