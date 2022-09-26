21aec6ee8d0fccff85c95a7226b1471a484419c3
Yannick Schurter <yannick.schurter@gmail.com>
2022-09-26 11:43:57 +0200

    feat: wait for the ready event before considering ezpaarse as being online

--- pm2.config.js
+++ pm2.config.js
@@ -10,6 +10,8 @@ module.exports = {
     script: 'server.js',
     exp_backoff_restart_delay: 100,
     kill_timeout: 5000,
+    wait_ready: true,
+    listen_timeout: 100000,
     max_memory_restart: '1500M',
     error_file: './logs/err.log',
     out_file: './logs/out.log',
