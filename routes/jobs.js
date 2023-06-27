207071750c32bbd96f45de08db05ca6276d832d6
Yannick Schurter <yannick.schurter@gmail.com>
2023-06-27 14:48:21 +0200

    fix(admin): promisify mongo calls in treatment routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -9,30 +9,24 @@ const { Router } = require('express');
 const app = Router();
 
 app.get('/:userId', auth.ensureAuthenticated(true), (req, res, next) => {
-  treatment.findAllByUser(req.params.userId, (err, result) => {
-    if (err) { return next(err); }
-
-    return res.json(result);
-  });
+  treatment.findAllByUser(req.params.userId)
+    .then(result => { res.status(200).json(result); })
+    .catch(err => next(err));
 });
 
 app.get('/', auth.ensureAuthenticated(true), auth.authorizeMembersOf('admin'), (req, res, next) => {
-  treatment.findAll((err, result) => {
-    if (err) { return next(err); }
-
-    return res.json(result);
-  });
+  treatment.findAll()
+    .then(result => { res.status(200).json(result); })
+    .catch(err => next(err));
 });
 
 app.post('/', bodyParser.urlencoded({ extended: true }), bodyParser.json(),
   auth.ensureAuthenticated(true), (req, res, next)  => {
     if (!req.body.userId || !req.body.uuid) { return next(Boom.badRequest('NoBodySet')); }
 
-    treatment.insert(req.body.userId, req.body.uuid, (err, result) => {
-      if (err) { return next(err); }
-
-      return res.status(200).json(result);
-    });
+    treatment.insert(req.body.userId, req.body.uuid)
+      .then(result => { res.status(200).json(result); })
+      .catch(err => next(err));
   });
 
 module.exports = app;
