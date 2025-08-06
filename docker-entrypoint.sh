191a4b66cd6284ced2b1e4c3764c645e1798d4e2
yannick.schurter <yannick.schurter@gmail.com>
2025-08-06 11:30:04 +0200

    build: add some logs in the docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 set -e
 
+echo "Updating repositories..."
+
 if ! [ -d middlewares/.git ]; then
   make middlewares-update
+else
+  echo "Directory 'middlewares' already exists"
 fi
 
 if ! [ -d resources/.git ]; then
   make resources-update
+else
+  echo "Directory 'resources' already exists"
 fi
 
 if ! [ -d platforms/.git ]; then
   make platforms-update
+else
+  echo "Directory 'platforms' already exists"
 fi
 
 if ! [ -d exclusions/.git ]; then
   make exclusions-update
+else
+  echo "Directory 'exclusions' already exists"
 fi
 
 exec "$@"
